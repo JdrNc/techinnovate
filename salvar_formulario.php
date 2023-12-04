81a4113ad8fddd612fe4df6e44b24f17edb06aa6
Jordan Correa <jordan.pintocorrea@hotmail.com>
2023-12-04 10:17:01 -0300

    added changes

--- salvar_formulario.php
+++ salvar_formulario.php
@@ -1,9 +1,9 @@
 <?php
 // Conectar ao banco de dados (substitua com suas próprias informações)
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "contato_db";
+$servername = "sql111.infinityfree.com";
+$username = "if0_35536988";
+$password = "L8C2cnxXhMiq";
+$dbname = "if0_35536988_formulario_contato";
 
 // Tentar realizar a conexão
 $conn = mysqli_connect($servername, $username, $password, $dbname);
